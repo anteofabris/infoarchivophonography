eed08a8d2c7c59de285f9ffc52c86c0cca521ed7
anteofabris <anteo8fabris@gmail.com>
2024-10-04 23:18:06 -0700

    project initialized; vite frontend, graphql backend

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const { loading, error, data } = useQuery(gql`
+    query ExampleQuery {
+      books {
+        title
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  const books = data.books;
+  console.log("books: ", books);
+
+  return (
+    <>
+      <h1>Infoarchivophone</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
