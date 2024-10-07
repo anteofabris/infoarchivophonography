b7f4982f293ffa595baae2f08ff9865865c367d6
anteofabris <anteo8fabris@gmail.com>
2024-10-06 18:47:55 -0700

    offline master setup

--- server/database/helpers/index.ts
+++ server/database/helpers/index.ts
@@ -11,9 +11,10 @@ export function deepExtractAndSort(docs: Array<any>) {
   let operation = docs.map((doc) => doc.lead_paragraph); // extract all first paragraphs
   operation = operation
     .join()
-    .replace(/["'’‘(),”“]/g, "")
-    .split(/[!.?:;](?=\s)/g); // clean all commas, apostrophes and quotes
-  operation = operation.map((s) => s.trim().toLowerCase()); // trim whitespace and make lowercase
+    .replace(/["'’‘(),”“]/g, "") // clean all commas, apostrophes and quotes
+    .split(/[!.?:;](?=\s)/g); // split along punctuation followed by space
+    operation = operation.map((s) => s.trim().toLowerCase()); // trim whitespace and make lowercase
+    operation = operation.map((s) => s.split(/[!.?:;]/g)).flat() // split along punctuation without a trailing space
   operation = operation.filter((s) => s.length > 5); // remove strings that would be musically too uninteresting
   operation = operation.sort(); // sort the strings
   return operation;
--- server/database/index.ts
+++ server/database/index.ts
@@ -4,36 +4,36 @@ import path from "path";
 import { deepExtractAndSort } from "./helpers";
 import { playPhrase } from "../../src/synth";
 import { createAsyncLoop } from "../../src/synth/loop";
+import testNews from "../../logs/newsResponse.json"
 
 export async function dbGetNewsByMonth(month: number, year: number) {
   // month is 0-indexed, so we will add 1 here
   // api call for data
-  let news;
-  try {
-    news = await axios.get(`https://api.nytimes.com/svc/archive/v1/${year}/${
-      month + 1
-    }.json?api-key=${process.env.VITE_NYT_API_KEY}
-  `);
-  } catch (err) {
-    console.log("error getting news: ", err, process.env.VITE_NYT_API_KEY);
-  }
-  const alphabeticalOrder = deepExtractAndSort(news.data.response.docs);
+  let news = testNews;
+  // try {
+  //   news = await axios.get(`https://api.nytimes.com/svc/archive/v1/${year}/${
+  //     month + 1
+  //   }.json?api-key=${process.env.VITE_NYT_API_KEY}
+  // `);
+  // } catch (err) {
+  //   console.log("error getting news: ", err, process.env.VITE_NYT_API_KEY);
+  // }
+  const alphabeticalOrder = deepExtractAndSort(testNews.response.docs);
   const writeObj = { sorted: alphabeticalOrder };
   const filepath = path.join(process.cwd(), "/logs/sortedNews.json");
-  const filepath2 = path.join(process.cwd(), "/logs/newsResponse.json");
+  // const filepath2 = path.join(process.cwd(), "/logs/newsResponse.json");
   fs.writeFile(filepath, JSON.stringify(writeObj), function (err) {
     if (err) {
       return console.log(err);
     }
     console.log("The sorted file was saved!");
   });
-  fs.writeFile(filepath2, JSON.stringify(news.data), function (err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("The data was saved!");
-  });
-  console.log("docs length: ", news.data.response.docs.length);
+  // fs.writeFile(filepath2, JSON.stringify(news.data), function (err) {
+  //   if (err) {
+  //     return console.log(err);
+  //   }
+  //   console.log("The data was saved!");
+  // });
   return alphabeticalOrder;
 }
 
@@ -41,7 +41,6 @@ export async function dbPlayData(data: string[], startingIndex: number) {
   // start a loop with a stop method
   // let loop = createAsyncLoop(playPhrase, data, startingIndex, 1000, 2000);
   // loop.start();
-
   // // return the stop method
   // return loop;
 }
